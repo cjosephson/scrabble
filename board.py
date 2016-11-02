d1553518cc207ea66a0d9700f8b861da898e5758
cjosephson <cjoseph@mit.edu>
2016-11-01 23:57:07 -0700

    Revert "started adding board multipliers"
    This reverts commit b046245d262419bee0aa1e69ced8f00742516508.

--- board.py
+++ board.py
@@ -4,57 +4,23 @@ import oracle
 import baseline
 
 class Board:
-    NORMAL = 'normal'
-    DOUBLEWORD = 'doubleword'
-    TRIPLEWORD = 'tripleword'
-    DOUBLELETTER = 'doubleletter'
-    TRIPLELETTER = 'tripleletter'
-
     def __init__(self):
         self.board=[]
         for i in xrange(15):
-            self.board.append([(' ', Board.NORMAL) for i in xrange(15)])
+            self.board.append([(' ') for i in xrange(15)])
         self.dictionary = {}
         #load in dictionary
         f = file('scrabblewords.txt','r')
         for w in f:
             self.dictionary[str.strip(w)] = True
 
-        #------------BONUS SQUARES------------------		
-	triplewords = [(0,0), (7,0), (14,0), (0,7), (14,7), (0,14), (7,14), (14,14)]	
-	for (x, y) in triplewords:
-	    self.board[x][y] = (' ', Board.TRIPLEWORD)
-	    
-	doublewords = [(1,1), (2,2), (3,3), (4,4), (1,13), (2,12), (3,11), (4,10),
-		       (13,1), (12,2), (11,3), (10,4), (13,13), (12,12), (11,11), (10,10),
-					   (7,7)]
-	for (x, y) in doublewords:
-	    self.board[x][y] = (' ', Board.DOUBLEWORD)
-			
-	tripleletters = [(5,1), (9,1), (1,5), (1,9), (5,13), (9,13), (13,5), (13,9),
-			 (5,5), (9,9), (5,9), (9,5)]
-	for (x, y) in tripleletters:
-	    self.board[x][y] = (' ', Board.TRIPLELETTER)
-		
-	doubleletters = [(3,0), (0,3), (11,0), (0,11), (3,14), (11,14), (14,3), (14,11),
-			 (2,6), (3,7), (2,8), (6,2), (7,3), (8,2), (6,12), (7,11), (8,12),
-			 (12,6), (11,7), (12,8), (6,6), (8,8), (6,8), (8,6)]
-	for (x, y) in doubleletters:
-	    self.board[x][y] = (' ', Board.DOUBLELETTER)
-	# #-----------------------------------------
-	
-
     def __str__(self):
-        mults = {Board.NORMAL: ' ', Board.DOUBLELETTER:'2', Board.DOUBLEWORD:'*',
-                 Board.TRIPLELETTER:'3', Board.TRIPLEWORD:'&'}
         s = ''
         for i in xrange(len(self.board)):
-            s += ("%02d"%(i))+": "+str([mults[b[1]] if b[0] == ' ' else  b[0]
-                                        for b in self.board[i]])+'\n'
+            s += ("%02d"%(i))+": "+str(self.board[i])+'\n'
         s+="     "
         for i in xrange(15):
             s+=("|%02d| "%(i))
-        s+="\nLegend: 2=double letter, *=double word, 3=triple letter, &=triple word"
         return s
 
     '''Given a word, a start point, and an orientation, return a score for
@@ -180,7 +146,4 @@ class Board:
         if ci < 15:
             col = [self.board[i][ci] for i in xrange(15)]
         return col
-
-    def insert(self, c, pos):
-        x,y = pos
-        self.board[x][y] = (c, self.board[x][y][1])
+    
